e137fe9cc8e676e5b21c334d555ec7a76640dcc4
Prechh <adamadrien21@gmail.com>
2022-03-16 16:23:09 +0100

    création barre Navigation responsiv

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuLogo = document.getElementsByClassName('bx-menu')[0]
+const navbarLinks = document.getElementsByClassName('navLinks')[0]
+
+menuLogo.addEventListener('click', () => {
+navbarLinks.classList.toggle('actived')
+})
