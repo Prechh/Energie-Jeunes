a91ca7169dd157f60550593df02754ee13ce141b
Prechh <adamadrien21@gmail.com>
2022-03-24 16:28:30 +0100

    mettre la barre de navigation fixe par rapport au défilement de la page

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const menuLogo = document.getElementsByClassName('bx-menu')[0]
 const navbarLinks = document.getElementsByClassName('navLinks')[0]
+const navBar =  document.getElementsByClassName('navBar')
 
 menuLogo.addEventListener('click', () => {
 navbarLinks.classList.toggle('actived')
 })
+
+function navBarScroll()
+{
+  if (document.documentElement.scrollTop > 50)
+  {
+    document.getElementById('navBar').className ="test";
+  }
+  else
+  {
+    document.getElementById('navBar').className="navBar";
+  }
+}
